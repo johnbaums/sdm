5b331ff1a7785bc554be8eb49bde49322a84caec
John <johnbaums@gmail.com>
2019-12-11 12:52:54 +1100

    Always return occurrences as sf objects
    Also add coords, fix typos, and modify return types for gbif

--- R/get_ala_occ.R
+++ R/get_ala_occ.R
@@ -1,44 +1,43 @@
-#' Get occurrence data from GBIF
+#' Get occurrence data from ALA
 #'
-#' Get occurrence data for a species from GBIF.
+#' Get occurrence data for a species from the Atlas of Living Australia.
 #'
 #' @param x Character. A taxon name.
-#' @param spatial Logical. If \code{TRUE}, return data as an \code{sf} object.
-#'   If \code{FALSE}, return as a \code{tbl_df}.
 #' @param target_crs One of: a \code{crs} object, an EPSG code passed as a 
 #'   number (e.g. \code{target_crs=4236} for WGS84), or a character string 
 #'   describing a projection as accepted by OGR (such as a PROJ4 string, e.g.
 #'   \code{target_crs='+init=epsg:4326'})
 #' @param return_meta Logical. If \code{TRUE}, return a list with occurrence 
-#'   data as well as associated metadata. If \code{FALSE}, only return 
+#'   data as well as associated metadata. If \code{FALSE} (default), only return 
 #'   occurrence data.
+#' @param quiet Logical. Suppress messages?
+#' @param ... Additional arguments passed to \code{\link[ALA4R]{occurrences}}.
 #' @return If \code{return_meta} is \code{TRUE}, a list with two elements: 
-#'   \code{data} (the occurrence data, as an \code{sf} or \code{tbl_df} object,
-#'   depending on \code{spatial}) and \code{meta} (associated metadata, a 
-#'   \code{tbl_df}). If \code{return_meta} is false, only the \code{data} 
-#'   element is returned, as \code{sf} or \code{tbl_df}.
-#' @details The underlying web service returns a maximum of 200,000 records. If
+#'   \code{data} (the occurrence data, as an \code{sf} object, and \code{meta} 
+#'   (associated metadata, a \code{tbl_df}). If \code{return_meta} is 
+#'   \code{FALSE}, only the \code{data} element is returned.
+#' @details The underlying web service returns a maximum of 500,000 records. If
 #'   a greater number of records are available for the requested taxon, the 
-#'   first 200,000 will be returned. Only records that have coordinates, and 
-#'   that are not flagged as having geospatial issues are requested. By default,
-#'   only the occurence data are returned - this behaviour can be overridden by
-#'   passing the \code{return} argument (see \code{\link[rgbif]{occ_search}}),
-#'   (e.g. to also return metadata).
+#'   first 500,000 will be returned. Only records that have coordinates, and 
+#'   that are not flagged as having geospatial issues are requested.
 #' @seealso get_gbif_occ
 #' @importFrom sf st_as_sf st_set_crs st_transform
 #' @importFrom dplyr as.tbl
+#' @importFrom ALA4R search_names occurrences
 #' @export
 #'
-get_ala_occ <- function(x, spatial = TRUE, target_crs = NULL, 
-                        return_meta = FALSE, ...) {
-  
-  if(!isTRUE(spatial) && !is.null(target_crs)) {
-    warning('target_crs not NULL but ignored because spatial is FALSE')
+get_ala_occ <- function(x, target_crs = NULL, return_meta = FALSE, quiet=FALSE, 
+                        ...) {
+ 
+  taxon <- ALA4R::search_names(x)
+  if(is.na(taxon$guid[1])) {
+    warning('No match for species: ', x)
+    return(NULL)
   }
-  sp <- ALA4R::search_names(x)
-  message(sprintf('Getting ALA data for %s (guid = %s)', 
-                  sp$acceptedConceptName, sp$acceptedConceptGuid))
-  
+  if(!quiet) {
+    message(sprintf('Getting ALA data for %s (guid = %s)', 
+                    taxon$acceptedConceptName, taxon$acceptedConceptGuid))
+  }
   args <- list(...)
   if(!'fq' %in% names(args) || !grepl('geospatial_kosher', args$fq)) {
     args$fq <- 'geospatial_kosher:true'
@@ -48,8 +47,10 @@ get_ala_occ <- function(x, spatial = TRUE, target_crs = NULL,
   
   # get record count
   n <- do.call(ALA4R::occurrences, 
-               c(taxon=sprintf('taxon_concept_lsid:%s', sp$acceptedConceptGuid),
+               c(taxon=sprintf('taxon_concept_lsid:%s', 
+                               taxon$acceptedConceptGuid),
                  record_count_only=TRUE, args))
+  if(!quiet) message(n, ' records.')
   if(n > 500000) {
     warning(sprintf(
       'More than 500000 records for species %s.\nReturning first 500000.', x
@@ -58,16 +59,16 @@ get_ala_occ <- function(x, spatial = TRUE, target_crs = NULL,
   
   # get records
   d <- do.call(ALA4R::occurrences, 
-               c(taxon=sprintf('taxon_concept_lsid:%s', sp$acceptedConceptGuid),
+               c(taxon=sprintf('taxon_concept_lsid:%s', 
+                               taxon$acceptedConceptGuid),
                  args))
   d <- lapply(d, dplyr::as.tbl)
-  if(isTRUE(spatial)) {
-    d$data <- sf::st_as_sf(d$data, coords=c('longitude', 'latitude'))
-    d$data <- sf::st_set_crs(d$data, 4326)
-    if(!is.null(target_crs)) {
-      d$data <- sf::st_transform(d$data, target_crs)
-    }
+  d$data <- sf::st_as_sf(d$data, coords=c('longitude', 'latitude'))
+  d$data <- sf::st_set_crs(d$data, 4326)
+  if(!is.null(target_crs)) {
+    d$data <- sf::st_transform(d$data, target_crs)
   }
+  d$data <- cbind(d$data, sf::st_coordinates(d$data))
   if(isTRUE(return_meta)) {
     class(d) <- c(class(d), 'occ_ala')
     return(d)
--- R/get_gbif_occ.R
+++ R/get_gbif_occ.R
@@ -1,42 +1,35 @@
 #' Get occurrence data from GBIF
 #'
-#' Get occurrence data for a species from GBIF.
+#' Get occurrence data for a species from the Global Biodiversity Information 
+#' Facility.
 #'
 #' @param x Character. A taxon name.
-#' @param spatial Logical. If \code{TRUE}, return data as an \code{sf} object.
-#'   If \code{FALSE}, return as a \code{tibble}.
 #' @param target_crs One of: a \code{crs} object, an EPSG code passed as a 
 #'   number (e.g. \code{target_crs=4236} for WGS84), or a character string 
 #'   describing a projection as accepted by OGR (such as a PROJ4 string, e.g.
 #'   \code{target_crs='+init=epsg:4326'}).
 #' @param ... Additional arguments passed to \code{\link[rgbif]{occ_search}}.
-#' @return A cleaned \code{sf} object (if \code{spatial} is \code{TRUE}, or 
-#'   \code{tibble} otherwise.
+#' @return An \code{sf} object containing occurrence data.
 #' @details The underlying web service returns a maximum of 200,000 records. If
 #'   a greater number of records are available for the requested taxon, the 
 #'   first 200,000 will be returned. Only records that have coordinates, and 
-#'   that are not flagged as having geospatial issues are requested. By default,
-#'   only the occurence data are returned - this behaviour can be overridden by
-#'   passing the \code{return} argument (see \code{\link[rgbif]{occ_search}}),
-#'   (e.g. to also return metadata).
+#'   that are not flagged as having geospatial issues are requested.
 #' @seealso get_ala_occ
-#' @importFrom sf st_as_sf st_set_crs st_transform
+#' @importFrom sf st_as_sf st_set_crs st_transform st_coordinates
 #' @importFrom dplyr as.tbl
+#' @importFrom rgbif name_suggest occ_search
 #' @export
 #'
-get_gbif_occ <- function(x, spatial = TRUE, target_crs=NULL, ...) {
-  require(dplyr)
-  require(rgbif)
+get_gbif_occ <- function(x, target_crs=NULL, ...) {
   args <-  list(...)
   # override occ_search defaults:
   if(!'limit' %in% names(args)) args$limit <- 200000
   if(!'hasCoordinate' %in% names(args)) args$hasCoordinate <- 'true'
   if(!'hasGeospatialIssue' %in% names(args)) args$hasGeospatialIssue <- 'false'
-  if(!'return' %in% names(args)) args$return <- 'data'
-  
-  if(!isTRUE(spatial) && !is.null(target_crs)) {
-    warning('target_crs not NULL but ignored because spatial is FALSE')
+  if('return' %in% names(args)) {
+    warning('`return` is unsupported and has been ignored.')
   }
+  args$return <- 'data'
   
   # occ_count args
   args_count <- list(georeferenced=args$hasCoordinate)
@@ -58,35 +51,17 @@ get_gbif_occ <- function(x, spatial = TRUE, target_crs=NULL, ...) {
   
   d <- do.call(rgbif::occ_search, c(taxonKey=key$key, args))
   # if no data...
-  if((args$return == 'data' && is.null(nrow(d))) || 
-     (args$return != 'data' && is.null(d$data))) {
+  if(is.null(nrow(d))) {
     warning('No data for species: ', key$canonicalName)
     return(invisible(NULL)) 
   }
-  if(args$return != 'data') {
-    if(isTRUE(spatial)) {
-      d$data <- sf::st_as_sf(
-        d$data, coords=c('decimalLongitude', 'decimalLatitude'))
-      d$data <- sf::st_set_crs(d$data, 4326)
-      if(!is.null(target_crs)) {
-        d$data <- sf::st_transform(d$data, target_crs)
-      }
-    } else {
-      d$data <- dplyr::as.tbl(d$data)
-    }
-    class(d$data) <- c(class(d$data), 'occ_gbif')
-  } else {
-    if(isTRUE(spatial)) {
-      d <- sf::st_as_sf(d, coords=c('decimalLongitude', 'decimalLatitude'))
-      d <- sf::st_set_crs(d, 4326)
-      if(!is.null(target_crs)) {
-        d <- sf::st_transform(d, target_crs)
-        d <- cbind(d, sf::st_coordinates(d))
-      }
-    } else {
-      d <- dplyr::as.tbl(d)
-    }
-    class(d) <- c(class(d), 'occ_gbif')
+
+  d <- sf::st_as_sf(d, coords=c('decimalLongitude', 'decimalLatitude'))
+  d <- sf::st_set_crs(d, 4326)
+  if(!is.null(target_crs)) {
+    d <- sf::st_transform(d, target_crs)
   }
+  d <- cbind(d, sf::st_coordinates(d))
+  class(d) <- c(class(d), 'occ_gbif')
   return(d)
 }
