d4ce9ce29a29adb4de1fe9dcf7412f49fc352276
John <johnbaums@gmail.com>
2019-12-11 17:12:53 +1100

    Add maxent_args dataset and docs

---
+++ R/maxent_args.R
@@ -0,0 +1,19 @@
+#' Maxent arguments
+#'
+#' Maxent arguments that can be used with the `rep_args` or `full_args` 
+#' arguments of [fit_maxent()].
+#'
+#' @docType data
+#'
+#' @usage data(maxent_args)
+#'
+#' @format An object of class `tbl`.
+#'
+#' @keywords datasets
+#'
+#' @source Maxent 3.4.0 GUI help
+#'
+#' @examples
+#' data(maxent_args)
+#' maxent_args[7, ]
+"maxent_args"
