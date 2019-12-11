111ee6ea70cd88eaba21aaccd90d37520d566880
John <johnbaums@gmail.com>
2019-12-11 17:13:23 +1100

    Update sample_background()

--- R/sample_background.R
+++ R/sample_background.R
@@ -11,7 +11,8 @@
 #'   sample.
 #' @param template_raster A `Raster*` object that defines the cell arrangement.
 #'   Cells that have `NA` value in this raster will not be included in the
-#'   background sample.
+#'   background sample. If `template_raster` has multiple layers, background
+#'   will only be sampled from cells that are non-NA across _all_ layers.
 #' @param target_background An `sf` or `sfc` object containing candidate points
 #'   to be used as background. This would typically be a "target species
 #'   background" sample. If `NULL` a random sample of `n` cells within the area
@@ -27,6 +28,7 @@
 #'   contained by an IBRA region, IBRA subregion or Koppen-Geiger zone in order
 #'   for background to be sampled from that region/subregion/zone. See also
 #'   explanation of `poly_type`, above.
+#' @param quiet Logical. Should messages be suppressed?
 #' @return An `sfc` object containing sampled background points.
 #' @importFrom raster raster alignExtent extent crop xyFromCell cellFromXY Which
 #' @importFrom fasterize fasterize
@@ -37,7 +39,7 @@
 #'
 sample_background <- function(n, occ, template_raster, target_background=NULL, 
                               buffer_width=NULL, poly_type=NULL, 
-                              adjacent_poly=FALSE) {
+                              adjacent_poly=FALSE, quiet=FALSE) {
   # if target_background not provided, background will be a random sample
   if(!is(occ, 'sf') & !is(occ, 'sfc')) stop('occ must be an sf or sfc object.')
   if(isTRUE(adjacent_poly)) warning('adjacent_poly ignored (not yet implemented)')
@@ -51,7 +53,7 @@ sample_background <- function(n, occ, template_raster, target_background=NULL,
     stop('template_raster must be a Raster* object or file path to a raster.')
   }
   if(is.null(poly_type) && is.null(buffer_width)) {
-    stop('At least one of buffer_width and poly_type must be specified.')
+    stop('At least one of buffer_width and/or poly_type must be specified.')
   }
   
   if(!is.null(buffer_width)) {
@@ -94,8 +96,12 @@ sample_background <- function(n, occ, template_raster, target_background=NULL,
   #                    deltay=raster::yres(template_raster))
   # i <- which(r$values==1)
   
-  r <- fasterize::fasterize(sf::st_as_sf(bg_poly_union), 
-                            raster::crop(template_raster, e))
+  template_raster_crop <- raster::crop(template_raster, e)
+  if(raster::nlayers(template_raster_crop) > 1) {
+    template_raster_crop <- !is.na(sum(template_raster_crop))
+    template_raster_crop[template_raster_crop==0] <- NA
+  }
+  r <- fasterize::fasterize(sf::st_as_sf(bg_poly_union), template_raster_crop)
   
   if(!is.null(target_background)) {
     bg_xy <- raster::xyFromCell(
@@ -104,22 +110,27 @@ sample_background <- function(n, occ, template_raster, target_background=NULL,
     if(nrow(bg_xy) == 0) {
       warning('No cells found.')
     } else if(nrow(bg_xy) > n) {
-      message(nrow(bg_xy), ' candidate background cells. Taking random sample',
-              ' of size ', n, '.')
+      if(!quiet) {
+        message(nrow(bg_xy), ' candidate background cells. Taking random sample',
+                ' of size ', n, '.')
+      }
       bg_xy <- bg_xy[sample(nrow(bg_xy), n), ]
     } else if(nrow(bg_xy) < n) {
       warning('Requested ', n, ' cells but only ', nrow(bg_xy), ' available.')
     }
   } else {
-    i <- raster::Which(r==1 & !is.na(template_raster), cells=TRUE)
+    i <- raster::Which(r==1 & !is.na(template_raster_crop), cells=TRUE)
     if(length(i) == 0) {
       warning('No cells found.')
     } else if(length(i) > n) {
-      message(nrow(bg_xy), ' candidate background cells. Taking random sample',
-              ' of size ', n, '.')
+      if(!quiet) {
+        message(length(i), ' candidate background cells. Taking random sample',
+                ' of size ', n, '.') 
+      }
       bg_xy <- raster::xyFromCell(r, sample(i, n))
     } else if(length(i) < n) {
       warning('Requested ', n, ' cells but only ', length(i), ' available.')
+      bg_xy <- raster::xyFromCell(r, i)
     }
   }
   
